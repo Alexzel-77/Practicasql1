b47dff23047517a6783f99a4b487aae5ca9284b0
Alexzel-77 <aezeledon@uamv.edu.ni>
2026-06-09 08:18:29 -0600

    Primer commit

---
+++ SQLQuery1.sql
@@ -0,0 +1,49 @@
+Create DataBase EmpresaSQL
+go
+
+use EmpresaSQL
+go
+
+Create Table TDepartamento(
+nDepartamentoID int identity(1,1) primary key,
+cNombreDepartamento varchar(100) not null unique
+);
+go
+
+Create Table TCargo(
+nCargoID int Identity(1,1) primary key,
+cNombreCargo varchar(100) not null unique
+);
+go
+
+Create Table TEmpleado(
+nEmpleadoID int identity(1,1) primary key,
+cNIF Varchar(20) unique,
+cNombre Varchar(100),
+cApellido varchar(100),
+nDepartamentoID int,
+nCargoID int,
+dFechaContratacion date default getdate(),
+nSalario decimal(18,2),
+constraint CK_Salario check (nSalario > 300));
+go
+
+ALTER TABLE TEmpleado
+ADD Constraint FK_Empleado_Departamento
+Foreign key(nDepartamentoID)
+References TDepartamento(nDepartamentoID);
+go
+
+Alter Table TEmpleado
+Add constraint FK_Empleado_Cargo
+Foreign Key(nCargoID)
+References TCargo(nCargoID);
+go
+
+
+Create Table TProyecto(
+nProyectoID int identity(1,1) primary key,
+cNombreProyecto varchar(100) not null,
+dFechaInicio date not null,
+dFechaFin date not null);
+go
